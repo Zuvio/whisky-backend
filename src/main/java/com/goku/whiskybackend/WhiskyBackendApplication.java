c008144feef9a096e55eb557e36d9bf547774ea6
zuvio <Githuberik81>
2018-11-12 10:50:32 +0100

    First Whisky Commit

---
+++ src/main/java/com/goku/whiskybackend/WhiskyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.goku.whiskybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyBackendApplication.class, args);
+	}
+}
